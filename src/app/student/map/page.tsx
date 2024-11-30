1c44b52703d118fb3d66761aab9f110a50d9690b
minbanyartalahtaw <minbanyartalahtaww@gmail.com>
2024-11-30 18:06:25 +0700

    add working map button

--- src/app/student/map/page.tsx
+++ src/app/student/map/page.tsx
@@ -1,9 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { useRouter } from "next/navigation";
 import style from "./page.module.css";
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
 export default function Map() {
   const router = useRouter();
+  const [SeeFloor, setSeeFloor] = useState(false);
+  const [SeeRoom, setSeeRoom] = useState(false);
+  const [floorNumber, setFloorNumber] = useState(0);
+  const srBuilding = {
+    name: "SR",
+    floors: [1, 2, 3, 4, 5],
+    rooms: [
+      ["101 - 106", "107 - 114"],
+      ["201 - 208", "209 - 218"],
+      ["301 - 324", "325 - 349"],
+      ["401 - 416", "417 - 431"],
+      ["SR - 501", "No Class"],
+    ],
+    buttonColor: "error",
+  };
+  const smBuilding = {
+    name: "SM",
+    floors: [1, 2, 3, 4, 5],
+    rooms: [
+      ["103 - 110", "111 - 118"],
+      ["204 - 212", "213 - 220"],
+      ["305 - 328", "329 - 352"],
+      ["407 - 415", "416 - 423"],
+      ["SM - 510", "No Class"],
+    ],
+    buttonColor: "success",
+  };
+
+  const sgBuilding = {
+    name: "SG",
+    floors: [1, 2, 3, 4, 5],
+    rooms: [
+      ["102 - 114", "115 - 119"],
+      ["202 - 211", "212 = 219"],
+      ["302 - 326", "327 - 350"],
+      ["404 - 419", "420 - 434"],
+      ["SG - 503", "No Class"],
+    ],
+    buttonColor: "primary",
+  };
+
+  const [building, setBuilding] = useState(srBuilding);
   return (
     <div
       style={{
@@ -14,17 +59,11 @@ export default function Map() {
         flexDirection: "column",
       }}
     >
-      <h1
-        style={{ color: "#eb5e28", fontFamily: "sans-serif", fontWeight: 100 }}
-      >
-        Map
-      </h1>
-
       <div
         className={style.mapContainer}
         style={{ border: "1px solid #eb5e28" }}
       >
-        <div className="map-left">
+        <div className={style.mapLeft}>
           <div className={style.buildingContainer}>
             <div className={style.SRbuildingContainer}>
               <div className={style.gate}></div>
@@ -52,65 +91,96 @@ export default function Map() {
         </div>
 
         <div className={style.mapRight}>
-          <div className={style.chooseBuilding}>
-            <h2>Choose the building</h2>
-            <div>
-              <p className="style.btn style.buildingButton"> SR</p>
-              <p className="stylebtn style.buildingButton">SM</p>
-              <p className="stylebtn style.buildingButton">SG</p>
+          <Typography variant="h3" className={style.title}>
+            map
+          </Typography>
+          <div className={style.rightContainer}>
+            <div className={style.chooseBuildingContainer}>
+              <Typography variant="h2">choose the building</Typography>
+              <div>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  onClick={() => {
+                    setSeeFloor(false);
+                    setSeeRoom(false);
+                    setBuilding(srBuilding);
+                    setSeeFloor(true);
+                  }}
+                >
+                  {srBuilding.name}
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="success"
+                  onClick={() => {
+                    setSeeFloor(false);
+                    setSeeRoom(false);
+                    setBuilding(smBuilding);
+                    setSeeFloor(true);
+                  }}
+                >
+                  {smBuilding.name}
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => {
+                    setSeeFloor(false);
+                    setSeeRoom(false);
+                    setBuilding(sgBuilding);
+                    setSeeFloor(true);
+                  }}
+                >
+                  {sgBuilding.name}
+                </Button>
+              </div>
             </div>
 
-            <div className="chooseFloor opicityZero">
-              <div className="sr-floor-container display-none">
-                <h2>Choose the floor</h2>
-                <div className="floorContainer">
-                  <p className="btn btn-outline-danger sr-floorNumber">1</p>
-                  <p className="btn btn-outline-danger sr-floorNumber">2</p>
-                  <p className="btn btn-outline-danger sr-floorNumber">3</p>
-                  <p className="btn btn-outline-danger sr-floorNumber">4</p>
-                  <p className="btn btn-outline-danger sr-floorNumber">5</p>
-                </div>
-              </div>
-
-              <div className="sm-floor-container display-none">
-                <h2>Choose the floor</h2>
-                <div className="floorContainer">
-                  <p className="btn btn-outline-success sm-floorNumber">1</p>
-                  <p className="btn btn-outline-success sm-floorNumber">2</p>
-                  <p className="btn btn-outline-success sm-floorNumber">3</p>
-                  <p className="btn btn-outline-success sm-floorNumber">4</p>
-                  <p className="btn btn-outline-success sm-floorNumber">5</p>
-                </div>
-              </div>
-
-              <div className="sg-floor-container display-none">
-                <h2>Choose the floor</h2>
-                <div className="floorContainer">
-                  <p className="btn btn-outline-warning sg-floorNumber">1</p>
-                  <p className="btn btn-outline-warning sg-floorNumber">2</p>
-                  <p className="btn btn-outline-warning sg-floorNumber">3</p>
-                  <p className="btn btn-outline-warning sg-floorNumber">4</p>
-                  <p className="btn btn-outline-warning sg-floorNumber">5</p>
-                </div>
+            <div
+              className={`${style.chooseFloorContainer} ${
+                SeeFloor ? "" : style.displayNone
+              }`}
+            >
+              <Typography variant="h2">choose the floor</Typography>
+              <div>
+                {building.floors.map((floor) => {
+                  return (
+                    <Button
+                      variant="contained"
+                      key={floor}
+                      color={building.buttonColor as any}
+                      onClick={() => {
+                        setSeeRoom(true);
+                        setFloorNumber(floor - 1);
+                      }}
+                    >
+                      {floor}
+                    </Button>
+                  );
+                })}
               </div>
             </div>
 
-            <div className="chooseRoom opicityZero">
-              <h2>Choose the room</h2>
-
-              <div className="sr-room display-none">
-                <p className="btn btn-outline-danger sr-room"></p>
-                <p className="btn btn-outline-danger sr-room"></p>
-              </div>
-
-              <div className="sm-room display-none">
-                <p className="btn btn-outline-success sm-room">123 - 123</p>
-                <p className="btn btn-outline-success sm-room">123 - 123</p>
-              </div>
-
-              <div className="sg-room display-none">
-                <p className="btn btn-outline-warning sg-room">123 - 123</p>
-                <p className="btn btn-outline-warning sg-room">123 - 123</p>
+            <div
+              className={`${style.chooseRoomContainer} ${
+                SeeRoom ? "" : style.displayNone
+              }`}
+            >
+              <Typography variant="h2">choose the room</Typography>
+              <div>
+                {building.rooms[floorNumber].map((room) => {
+                  return (
+                    <Button
+                      key={room}
+                      variant="contained"
+                      color={building.buttonColor as any}
+                    >
+                      {room}
+                    </Button>
+                  );
+                })}
+                ;
               </div>
             </div>
           </div>
@@ -123,7 +193,7 @@ export default function Map() {
           justifyContent: "center",
           alignItems: "center",
           gap: "10px",
-          marginTop: "100px",
+          marginTop: "60px",
           color: "#E63946",
           cursor: "pointer",
         }}
